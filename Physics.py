268f57dd02d694ba26008430410b2a5f7e027e58
SuperPotion1o1 <114557061+SuperPotion1o1@users.noreply.github.com>
2022-09-29 20:19:05 +0530

    Create Physics.py

---
+++ Physics.py
@@ -0,0 +1,11 @@
+Magnetism_and_MatterQ = ['A magnetic compass needle oscillates 30 times per minute at a place where the dip is 45°, and 40 times per minute where the dip is 30°. If B1 and B2 are, respectively the total magnetic field due to the earth at the two places, then the ratio B1 /B2 is best given by']
+            
+Magnetism_and_MatterA = ['0.7']
+            
+Magnetism_and_MatterB = ['3.6']
+        
+Magnetism_and_MatterC = ['1.8']
+          
+Magnetism_and_MatterD = ['2.2']   
+
+Magnetism_and_MatterSOLUTION = [Magnetism_and_MatterA[0]]        
