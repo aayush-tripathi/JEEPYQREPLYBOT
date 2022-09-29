c31448502e3a0931666c8ba86da6d36b34b07d0f
SuperPotion1o1 <114557061+SuperPotion1o1@users.noreply.github.com>
2022-09-29 20:24:31 +0530

    Update Physics.py

--- Physics.py
+++ Physics.py
@@ -1,11 +1,12 @@
-Magnetism_and_MatterQ = ['A magnetic compass needle oscillates 30 times per minute at a place where the dip is 45°, and 40 times per minute where the dip is 30°. If B1 and B2 are, respectively the total magnetic field due to the earth at the two places, then the ratio B1 /B2 is best given by']
+Magnetism_and_MatterQ = ['A magnetic compass needle oscillates 30 times per minute at a place where the dip is 45°, and 40 times per minute where the dip is 30°. If B1 and B2 are, respectively the total magnetic field due to the earth at the two places, then the ratio B1 /B2 is best given by',
+                        'A magnetic dipole is acted upon by two magnetic fields which are inclined to each other at an angle of 75°. One of the fields has a magnitude of 15 mT. The dipole attains stable equilibrium at an angle of 30° with this field. The magnitude of the other field (in mT) is close to']
             
-Magnetism_and_MatterA = ['0.7']
+Magnetism_and_MatterA = ['0.7','1']
             
-Magnetism_and_MatterB = ['3.6']
+Magnetism_and_MatterB = ['3.6','11']
         
-Magnetism_and_MatterC = ['1.8']
+Magnetism_and_MatterC = ['1.8','36']
           
-Magnetism_and_MatterD = ['2.2']   
+Magnetism_and_MatterD = ['2.2','1060']   
 
-Magnetism_and_MatterSOLUTION = [Magnetism_and_MatterA[0]]        
+Magnetism_and_MatterSOLUTION = [Magnetism_and_MatterA[0],Magnetism_and_MatterB[1]]        
