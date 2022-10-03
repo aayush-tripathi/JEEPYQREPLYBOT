2eec43149d80eafa0b296f7e370c64a4100561db
SuperPotion1o1 <114557061+SuperPotion1o1@users.noreply.github.com>
2022-10-03 09:12:28 +0530

    Create Mathematics.py

---
+++ subjects/Mathematics.py
@@ -0,0 +1,11 @@
+PnCQ = ['Sum of all five digits naturaal numbers formed using all the digits {0,1,2,3,4}'] 
+
+PnCA = ['2599980']
+
+PnCB = ['2666640']            
+
+PnCC = ['1999980']
+
+PnCD = ['none of these']
+
+PnCSOLUTION = [PnCA[0]]            
